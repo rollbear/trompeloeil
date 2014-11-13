185dcdb4198c69ed0f8b37fbc91e598ad207e32d
Bjorn Fahller <bjorn@fahller.se>
2014-11-13 17:38:17 +0100

    copyright/license-blurb everywhere

--- compilation_errors/negative_limit_interval_second.cpp
+++ compilation_errors/negative_limit_interval_second.cpp
@@ -1,3 +1,16 @@
+/*
+ * Trompeloeil C++ mocking framework
+ *
+ * Copyright Björn Fahller 2014
+ *
+ *  Use, modification and distribution is subject to the
+ *  Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Project home: https://github.com/rollbear/trompeloeil
+ */
+
 //In TIMES the first value must not exceed the second
 #include "../trompeloeil.hpp"
 
