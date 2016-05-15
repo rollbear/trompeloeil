a3f7c79eb6e513c3082072ffd43b0b4af45a393d
Bjorn Fahller <bjorn@fahller.se>
2016-05-15 11:45:35 +0200

    err msg tests for multiple seq in REQUIRE_DESTRUCTION

---
+++ compilation_errors/multiple_sequences_destruction.cpp
@@ -0,0 +1,29 @@
+/*
+ * Trompeloeil C++ mocking framework
+ *
+ * Copyright Björn Fahller 2014
+ *
+ *  Use, modification and distribution is subject to the
+ *  Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Project home: https://github.com/rollbear/trompeloeil
+ */
+
+//Multiple IN_SEQUENCE does not make sense. You can list several sequence
+#include "../trompeloeil.hpp"
+
+struct MS
+{
+  MAKE_MOCK0(f, int());
+};
+
+int main()
+{
+  trompeloeil::sequence seq;
+  MS obj;
+  REQUIRE_DESTRUCTION(obj)
+    .IN_SEQUENCE(seq)
+    .IN_SEQUENCE(seq);
+}
