a518c6744acdd04df6807f6f1e471fda81ed5816
Bjorn Fahller <bjorn@fahller.se>
2016-05-26 21:42:06 +0200

    Adapt to c++11 rules for constexpr funcs due to g++ 4.9 non-compliance

--- trompeloeil.hpp
+++ trompeloeil.hpp
@@ -440,7 +440,7 @@ namespace trompeloeil
   constexpr inline std::false_type is_output_streamable_(...) { return {}; }
 
   template <typename T>
-  constexpr inline auto is_output_streamable_(T* t) -> decltype((std::declval<std::ostream&>() << *t),std::true_type{}) { ignore(t); return {}; }
+  constexpr inline auto is_output_streamable_(T* t) -> decltype((std::declval<std::ostream&>() << *t),std::true_type{}) { return (ignore(t),std::true_type{}); }
 
   template <typename T>
   constexpr auto is_output_streamable() { return is_output_streamable_(static_cast<T*>(nullptr)); }
