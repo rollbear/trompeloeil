b07c4424c3b874417b85c26fe06c235147b1acd6
Bjorn Fahller <bjorn@fahller.se>
2016-10-02 18:48:55 +0200

    Fixed g++ 4.9 issue caused by VS fix

--- trompeloeil.hpp
+++ trompeloeil.hpp
@@ -1878,11 +1878,7 @@ namespace trompeloeil
     template <typename P, typename V>
     static constexpr std::false_type func(...) { return {}; }
     template <typename P, typename V>
-    static constexpr auto func(P* p, V* v) -> decltype((*p == *v), std::true_type{})
-    {
-      ::trompeloeil::ignore(p,v); // silence unmotivated VisualStudio warning
-      return {};
-    }
+    static constexpr auto func(P* p, V* v) -> decltype((*p == *v), std::true_type{});
     static constexpr auto value = decltype(func<T, U>(nullptr, nullptr))::value;
     // The obvious solution, to just call func<T,U>(0,0) gives true_type* in VS!!!
   };
