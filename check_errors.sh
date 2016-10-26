047fdb2ced991a36f40a3b286c3ac7ddca4dac2d
Bjorn Fahller <bjorn@fahller.se>
2016-10-26 20:14:03 +0200

    temporarily display compilation errors on travis

--- check_errors.sh
+++ check_errors.sh
@@ -17,11 +17,11 @@ PASS=$'\E[32mPASS\E[0m'
 FAIL=$'\E[1;31mFAIL\E[0m'
 FAILURES=0
 
-#echo "CXX=$CXX"
-#echo "CXXFLAGS=$CXXFLAGS"
-#echo "CPPFLAGS=$CPPFLAGS"
+echo "CXX=$CXX"
+echo "CXXFLAGS=$CXXFLAGS"
+echo "CPPFLAGS=$CPPFLAGS"
 
-#${CXX} --version
+${CXX} --version
 
 cd compilation_errors
 
@@ -29,7 +29,7 @@ for f in *.cpp
 do
   RE=$(sed -n 's:^//\(.*\)$:\1:g;T;P' < $f)
   printf "%-45s" $f
-  ${CXX} ${CXXFLAGS} ${CPPFLAGS} -std=c++14 $f -c |& egrep -q "$RE" && echo $PASS && continue || echo $FAIL && false
+  ${CXX} ${CXXFLAGS} ${CPPFLAGS} -std=c++14 $f -c |& egrep  "$RE" && echo $PASS && continue || echo $FAIL && false
   FAILURES=$((FAILURES+$?))
 done
 exit $FAILURES
