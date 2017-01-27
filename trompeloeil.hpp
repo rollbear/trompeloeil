a3923c2a81936433be94086e0777516759c4c6c9
Bjorn Fahller <bjorn@fahller.se>
2017-01-27 21:27:35 +0100

    Bumped (C) year

--- trompeloeil.hpp
+++ trompeloeil.hpp
@@ -1,7 +1,7 @@
 /*
  * Trompeloeil C++ mocking framework
  *
- * Copyright Björn Fahller 2014-2016
+ * Copyright Björn Fahller 2014-2017
  *
  *  Use, modification and distribution is subject to the
  *  Boost Software License, Version 1.0. (See accompanying
