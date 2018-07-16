aa302b71a34a0ce430327a1b6e907cd73e9eae3b
Andrew Paxie <cpp.scribe@gmail.com>
2018-07-16 19:25:38 +1200

    Fix check_errors.sh and detect failure in Travis

--- check_errors.sh
+++ check_errors.sh
@@ -12,20 +12,21 @@
 #
 # Project home: https://github.com/rollbear/trompeloeil
 #
-
 PASS="\033[32mPASS\033[0m"
 FAIL="\033[1;31mFAIL\033[0m"
 FAILURES=0
 
-#echo "CXX=$CXX"
-#echo "CXXFLAGS=$CXXFLAGS"
-#echo "CPPFLAGS=$CPPFLAGS"
+# echo "CXX=$CXX"
+# echo "CXXFLAGS=$CXXFLAGS"
+# echo "CPPFLAGS=$CPPFLAGS"
 
 # Default CXXFLAGS to -std=c++14 if not set in the environment
 # for backward compatibility.
-CXXFLAGS=${CXXFLAGS-"-std=c++14"}
+CXXFLAGS=${CXXFLAGS:-"-std=c++14"}
 
-#${CXX} --version
+# echo "CXXFLAGS is now $CXXFLAGS"
+
+# ${CXX} --version
 cd compilation_errors
 SCRIPT='
 s:^//\(.*\)$:\1:g
@@ -38,7 +39,9 @@ for f in *.cpp
 do
   RE=`sed -n "$SCRIPT" < $f`
   printf "%-45s" $f
+  # echo "RE=$RE"
   ${CXX} ${CXXFLAGS} ${CPPFLAGS} -I ../include $f -c 2>&1 | egrep -q "${RE}" && echo ${PASS} && continue || echo ${FAIL} && false
   FAILURES=$((FAILURES+$?))
 done
+# echo "FAILURES=$FAILURES"
 exit $FAILURES
