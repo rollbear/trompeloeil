4e5f74e194b6a78db5e0ec8997caa1ae28952643
Andrew Paxie <cpp.scribe@gmail.com>
2021-06-13 16:30:48 +1200

    Issue 236: Fix -Wreserved-identifer warning from clang++-13

--- include/trompeloeil.hpp
+++ include/trompeloeil.hpp
@@ -439,34 +439,36 @@ namespace trompeloeil
      * ISO/IEC JTC1 SC22 WG21 N3656, 18 April 2013.
      * Available: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3656.htm
      * Accessed: 14 June 2017
+     *
+     * Renamed types to avoid the use of reserved identifiers.
      */
     template <class T>
-    struct _Unique_if
+    struct unique_if
     {
-      typedef std::unique_ptr<T> _Single_object;
+      typedef std::unique_ptr<T> single_object;
     };
 
     template <class T>
-    struct _Unique_if<T[]>
+    struct unique_if<T[]>
     {
-      typedef std::unique_ptr<T[]> _Unknown_bound;
+      typedef std::unique_ptr<T[]> unknown_bound;
     };
 
     template <class T, size_t N>
-    struct _Unique_if<T[N]>
+    struct unique_if<T[N]>
     {
-      typedef void _Known_bound;
+      typedef void known_bound;
     };
 
     template <class T, class... Args>
-    typename _Unique_if<T>::_Single_object
+    typename unique_if<T>::single_object
     make_unique(Args&&... args)
     {
       return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
     }
 
     template <class T>
-    typename _Unique_if<T>::_Unknown_bound
+    typename unique_if<T>::unknown_bound
     make_unique(size_t n)
     {
       typedef typename std::remove_extent<T>::type U;
@@ -474,7 +476,7 @@ namespace trompeloeil
     }
 
     template <class T, class... Args>
-    typename _Unique_if<T>::_Known_bound
+    typename unique_if<T>::known_bound
     make_unique(Args&&...) = delete;
 
     /* <type_traits> */
